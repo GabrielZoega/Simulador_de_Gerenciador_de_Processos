8a841e26efa87b8890026dc2cca0f85469f5923a
GabrielZoega <gabriel.zoega10@gmail.com>
2024-07-21 08:44:20 -0300

    Adicionando a estrutura de dados da tabela de processos e do processo
    Co-authored-by: Ana Flávia Marcondes Bomfim <AnaMBomfim@users.noreply.github.com>

---
+++ headers/tabelaDeProcessos.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Essa definição talvez seja movida para um arquivo .h separado pra o processo
+typedef struct processo{
+    int idProcesso;
+    int idProcessoPai;
+    int* programCounter;
+    //dados* dados; teoricamente é um vetor de estruturas de dados (tá escrito assim na especificação), não sabemos extamanete oq é isso
+    int prioridade;
+    char estado;
+    int inicioTempo;
+    int tempoUsadoCPU;
+
+};
+
+// A tabela de Processos será um vetor do tipo processo
+typedef struct tabelaDeProcessos {
+    processo processo[50]; // Esse tamanho pode mudar dependendo dos testes mais tarde
+};
+
+
